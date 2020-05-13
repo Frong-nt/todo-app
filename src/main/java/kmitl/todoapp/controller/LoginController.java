54835c4a772eb4d4dfcff00c3ce926fb808ed767
Natthawut Tachasriburapha <natthawuttachasriburapha@MacBook-Pro-khxng-Natthawut.local>
2020-05-13 22:58:06 +0700

    fixed cors v2

--- src/main/java/kmitl/todoapp/controller/LoginController.java
+++ src/main/java/kmitl/todoapp/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-@CrossOrigin(origins = "*", maxAge = 3600)
+
 @RestController
 @RequestMapping(path="/login")
 public class LoginController  {
